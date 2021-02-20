3a41d5be59e93491f75f5a0742a11de8305a53b0
Michael <64799584+Michael-not@users.noreply.github.com>
2021-02-20 17:12:55 +0200

    Add files via upload

---
+++ Intro_to_Numpy(1).py
@@ -0,0 +1,17 @@
+import numpy as np
+from numpy import pi
+#1.1 Задаем размер масива
+size = 11
+#1.2 Создаем нулевой масив заданого размера
+arr= np.zeros([size,size])
+#1.3 Выводим созданный масив
+print(arr,'\n')
+#1.4 Заменяем диагонали с помощью двух циклов
+for i in range(size):
+    for j in range(size):
+        if j == i:
+            arr[i][j] = 1
+        elif j == (size-1-i):
+            arr[i][j] = 1
+#1.5 Выводим созданный масив
+print(arr)
